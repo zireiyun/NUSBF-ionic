faea96eda0ff75e9ea7e13280fb684f6cd8548aa
zireiyun <zirei0303@hotmail.com>
2016-08-24 01:43:33 +0800

    last  commit (hopefully :new_moon_with_face:)

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3912,7 +3912,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true,"app_id":"0bb8679f","api_key":"2d5b23d9d5a61d52463a9fe44b71ee84d490917c13d0b164"}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"dev_push":true,"app_id":"0bb8679f","api_key":"2d5b23d9d5a61d52463a9fe44b71ee84d490917c13d0b164"}; return { get: function(setting) { if (settings[setting]) {return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {
